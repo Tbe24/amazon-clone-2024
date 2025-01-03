63f22439f0ac4f0ef528bae4ff379e376f0827d1
ttesfaye629@gmail.com <127722108+Tbe24@users.noreply.github.com>
2025-01-03 08:23:29 +0300

    router maintained

--- amazon-clone/src/Router.jsx
+++ amazon-clone/src/Router.jsx
@@ -9,21 +9,26 @@ import Cart from "./Pages/Cart/Cart";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+
 const stripePromise = loadStripe(
   "pk_test_51QWgJt2KfKTZiI0s178aucBU1YcMSP35zB1CoXPk9DkFJ8M1ErXcxNIAmiorkun4FjKG7pwNq8kNOeHPkf91lYxf00OiVC9sqO"
 );
-import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+
+// Dynamically set basename based on environment
+const isProduction = process.env.NODE_ENV === "production";
+const basename = isProduction ? "/amazon-clone-2024" : "/";
 
 function Routing() {
   return (
-    <Router>
+    <Router basename={basename}>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
         <Route
           path="/payments"
           element={
-            <ProtectedRoute msg="you must login to pay" redirect="/payments">
+            <ProtectedRoute msg="You must login to pay" redirect="/auth">
               <Elements stripe={stripePromise}>
                 <Payment />
               </Elements>
@@ -33,12 +38,14 @@ function Routing() {
         <Route
           path="/orders"
           element={
-            <ProtectedRoute msg="you must login to access your orders" redirect="/orders">
+            <ProtectedRoute
+              msg="You must login to access your orders"
+              redirect="/auth"
+            >
               <Orders />
             </ProtectedRoute>
           }
         />
-        {/* <Route path="/category/:C" element={<Results />} /> */}
         <Route path="/category/:categoryName" element={<Results />} />
         <Route path="/products/:productId" element={<ProductDetail />} />
         <Route path="/cart" element={<Cart />} />
@@ -46,4 +53,5 @@ function Routing() {
     </Router>
   );
 }
+
 export default Routing;
